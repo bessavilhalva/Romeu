2e469323824002b7a1176a5d73c063f3825b2f52
Romeu <bessavilhalva@gmail.com>
2017-08-28 16:10:29 -0300

    Tela de splash

---
+++ src/pages/login/login.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  splash = true;
+  tabBarElement: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.tabBarElement = document.querySelector('.tabbar');
+  }
+
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+    this.splash = false;
+    this.tabBarElement.style.display = 'flex';
+    }, 4000);
+  }
+
+}
